aa79b0b4eeeb9e7e0556f4169dc363421a347925
SKPatil18 <85570532+SKPatil18@users.noreply.github.com>
2021-06-09 20:34:31 +0530

    Add files via upload

---
+++ Python/PNRN.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void sum()
+{
+    int a,b,c;
+    cout<<"enter two numbers:"<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"the addition is:"<<c;
+
+}
+
+int main()
+{
+    sum();
+    return 0;
+}
+

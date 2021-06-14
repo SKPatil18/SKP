1f2c2176b21e08d50b0cd1032b4d7a1fffd28e31
SKPatil18 <85570532+SKPatil18@users.noreply.github.com>
2021-06-14 22:03:13 +0530

    Update whatsmsg.py

--- Python/whatsmsg.py
+++ Python/whatsmsg.py
@@ -2,3 +2,4 @@
 
 import pywhatkit
 pywhatkit.sendwhatmsg_instantly("+91mobile no","message you want to send")
+https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2021-03/R/eclipse-java-2021-03-R-win32-x86_64.zip
